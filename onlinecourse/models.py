95fdc321e848c99c6a08ae30f278141e02768dba
TheDevJoeT <thedevjoet@gmail.com>
2026-01-21 03:01:15 -0500

    task2 admin file update

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -99,10 +99,8 @@ class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     grade = models.IntegerField(default=50)
-
     def __str__(self):
         return "Question: " + self.content
-
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
